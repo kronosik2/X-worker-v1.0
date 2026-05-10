871709123e4182aa71300c6d9f34641fc4633b63
kronosik2 <kronosik2@gmail.com>
2026-05-10 15:11:56 +0500

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/PROJECT-X-worker',
+  assetPrefix: '/PROJECT-X-worker',
+};
+
+export default nextConfig;
